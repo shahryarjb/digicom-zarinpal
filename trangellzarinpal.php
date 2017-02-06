15645d128b19edc4ff4a052a81344628080fd4ae
mojtaba-naserei <mojtaba.naserei@gmail.com>
2017-02-06 16:38:26 +0330

    نسخه اول
    تست شده

---
+++ trangellzarinpal.php
@@ -0,0 +1,242 @@
+<?php
+/**
+ * @package     Joomla - > Site and Administrator payment info
+ * @subpackage  com_Digicom
+ * @subpackage 	trangell_Zarinpal
+ * @copyright   trangell team => https://trangell.com
+ * @copyright   Copyright (C) 20016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+defined('_JEXEC') or die;
+
+require_once(dirname(__FILE__) . '/trangellzarinpal/helper.php');
+if (!class_exists ('checkHack')) {
+	require_once( dirname(__FILE__) . '/trangellzarinpal/trangell_inputcheck.php');
+}
+
+class plgDigiCom_PayTrangellZarinpal extends JPlugin
+{
+	protected $autoloadLanguage = true;
+
+	function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		$this->responseStatus= array (
+			'Completed' => 'A',
+			'Pending' 	=> 'P',
+			'Failed' 		=> 'P',
+			'Denied' 		=> 'P',
+			'Refunded'	=> 'RF'
+		);
+	}
+	
+	public function onDigicomSidebarMenuItem()
+	{
+		$pluginid = $this->getPluginId('trangellzarinpal','digicom_pay','plugin');
+		$params 	= $this->params;
+		$link 		= JRoute::_("index.php?option=com_plugins&client_id=0&task=plugin.edit&extension_id=".$pluginid);
+		return '<a target="_blank" href="' . $link . '" title="trangellzarinpal" id="plugin-'.$pluginid.'">' . 'trangellzarinpal' . '</a>';
+	}
+	
+	function getPluginId($element,$folder, $type)
+	{
+	    $db = JFactory::getDBO();
+	    $query = $db->getQuery(true);
+	    $query
+	        ->select($db->quoteName('a.extension_id'))
+	        ->from($db->quoteName('#__extensions', 'a'))
+	        ->where($db->quoteName('a.element').' = '.$db->quote($element))
+	        ->where($db->quoteName('a.folder').' = '.$db->quote($folder))
+	        ->where($db->quoteName('a.type').' = '.$db->quote($type));
+
+	    $db->setQuery($query);
+	    $db->execute();
+	    if($db->getNumRows()){
+	        return $db->loadResult();
+	    }
+	    return false;
+	}
+
+	function buildLayoutPath($layout)
+	{
+		if(empty($layout)) $layout = "default";
+		$core_file 	= dirname(__FILE__) . '/' . $this->_name . '/tmpl/' . $layout . '.php';
+		return $core_file;
+	}
+
+	function buildLayout($vars, $layout = 'default' )
+	{
+		ob_start();
+		$layout = $this->buildLayoutPath($layout);
+		include($layout);
+		$html = ob_get_contents();
+		ob_end_clean();
+		return $html;
+	}
+
+	function onDigicom_PayGetHTML($vars,$pg_plugin)
+	{
+		if($pg_plugin != $this->_name) return;
+		$vars->custom_name= $this->params->get( 'plugin_name' );
+		$configs = JComponentHelper::getComponent('com_digicom')->params;
+		//$vars->custom_email=$this->params->get( 'plugin_mail' );
+		$price = intval(str_replace(".","",plgDigiCom_PayTrangellZarinpalHelper::getPayerPrice($vars->order_id)));
+		//=========================================================
+		$app	= JFactory::getApplication();
+		$Amount = $price/10; // Toman 
+		$Description = 'خرید محصول از فروشگاه   '. $configs->get('store_name','DigiCom Store'); 
+		$Email = ''; 
+		$Mobile = ''; 
+		$CallbackURL = $vars->return;
+
+		try {
+			 $client = new SoapClient('https://www.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']); 	
+			//$client = new SoapClient('https://sandbox.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']); // for local
+
+			$result = $client->PaymentRequest(
+				[
+				'MerchantID' => $this->params->get('merchant_id'),
+				'Amount' => $Amount,
+				'Description' => $Description,
+				'Email' => $Email,
+				'Mobile' => $Mobile,
+				'CallbackURL' => $CallbackURL,
+				]
+			);
+			
+			$resultStatus = abs($result->Status); 
+			if ($resultStatus == 100) {
+				$vars->urls = 'https://www.zarinpal.com/pg/StartPay/'.$result->Authority; 
+				$html = $this->buildLayout($vars);
+				return $html;
+			//Header('Location: https://sandbox.zarinpal.com/pg/StartPay/'.$result->Authority); // for local/
+			} else {
+				$msg= plgDigiCom_PayTrangellZarinpalHelper::getGateMsg('error'); 
+				$app->enqueueMessage($msg .' خطای '. $resultStatus, 'error');	
+				return false;
+			}
+		}
+		catch(\SoapFault $e) {
+			$msg= plgDigiCom_PayTrangellZarinpalHelper::getGateMsg('error'); 
+			$app->enqueueMessage($msg , 'error');	
+			return false;
+		}	
+	}
+
+	function onDigicom_PayGetInfo($config)
+	{
+		if(!in_array($this->_name,$config)) return;
+		$obj 		= new stdClass;
+		$obj->name 	= $this->params->get( 'plugin_name' );
+		$obj->id	= $this->_name;
+		return $obj;
+	}
+
+	function onDigicom_PayProcesspayment($data)
+	{
+		$processor = JFactory::getApplication()->input->get('processor','');
+		if($processor != $this->_name) return;
+		$app	= JFactory::getApplication();	
+		$jinput = $app->input;
+		$orderId = $jinput->get->get('order_id', '0', 'INT');
+		$price = intval(str_replace(".","",plgDigiCom_PayTrangellZarinpalHelper::getPayerPrice($orderId)));
+		$trackingCode = "";
+		//========================================================================
+
+		$Authority = $jinput->get->get('Authority', '0', 'INT');
+		$status = $jinput->get->get('Status', '', 'STRING');
+			
+		if (checkHack::checkString($status)){
+			if ($status == 'OK') {
+				try {
+				     $client = new SoapClient('https://www.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']); 
+					//$client = new SoapClient('https://sandbox.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']); // for local
+
+					$result = $client->PaymentVerification(
+						[
+							'MerchantID' => $this->params->get('merchant_id'),
+							'Authority' => $Authority,
+							'Amount' => $price/10,
+						]
+					);
+					$resultStatus = abs($result->Status); 
+					if ($resultStatus == 100) {
+						$payment_status = $this->translateResponse('Completed');
+						$db = JFactory::getDbo();
+						$query = $db->getQuery(true);
+						$fields = array($db->qn('amount_paid') . ' = ' .$db->q($price) );
+						$conditions = array($db->qn('id') . ' = ' . $db->q(intval($orderId)));
+						$query->update($db->qn('#__digicom_orders'))->set($fields)->where($conditions);
+						$db->setQuery((string)$query); 
+						$db->execute(); 
+						$trackingCode = $result->RefID;
+						$msg= plgDigiCom_PayTrangellZarinpalHelper::getGateMsg($resultStatus); 
+						$message = "کد پیگیری".$trackingCode;
+						$app->enqueueMessage($msg.'<br/>' .$message, 'message');
+					} 
+					else {
+						$msg= plgDigiCom_PayTrangellZarinpalHelper::getGateMsg($resultStatus); 
+						$app->enqueueMessage($msg, 'error');
+						$payment_status = $this->translateResponse('Failed');	
+					}
+				}
+				catch(\SoapFault $e) {
+					$msg= plgDigiCom_PayTrangellZarinpalHelper::getGateMsg('error'); 
+					$app->enqueueMessage($msg, 'error');
+					$payment_status = $this->translateResponse('Failed');	
+				}
+			}
+			else {
+				$msg= plgDigiCom_PayTrangellZarinpalHelper::getGateMsg(intval(17)); 
+				$app->enqueueMessage($msg, 'error');
+				$payment_status = $this->translateResponse('Pending');
+			}
+		}
+		else {
+			$msg= plgDigiCom_PayTrangellZarinpalHelper::getGateMsg('hck2'); 
+			$app->enqueueMessage($msg, 'error');
+			$payment_status = $this->translateResponse('Failed');
+		}
+
+		$data['payment_status'] = $payment_status;
+		if(!isset($data['payment_status']))
+		{
+			$info = array('raw_data'	=>	$data);
+			$this->onDigicom_PayStorelog($this->_name, $info);
+		}
+
+		$result = array(
+			'transaction_id'	=>	$this->getUniqueTransactionId($orderId),
+			'order_id'				=>	$orderId,
+			'status'					=>	$payment_status,
+			'raw_data'				=>	json_encode($data),
+			'trackingcode'			=>	"کد پیگیری".$trackingCode,
+			'processor'				=>	$processor
+		);
+		return $result;
+	}
+
+	function translateResponse($invoice_status){
+
+		foreach($this->responseStatus as $key=>$value)
+		{
+			if($key==$invoice_status)
+			return $value;
+		}
+	}
+
+	function onDigicom_PayStorelog($name, $data)
+	{
+		if($name != $this->_name) return;
+		plgDigiCom_PayTrangellZarinpalHelper::Storelog($this->_name,$data);
+	}
+
+	function getUniqueTransactionId($order_id){
+		$uniqueValue = $order_id.time();
+		$long = md5(uniqid($uniqueValue, true));
+		return substr($long, 0, 15);
+	}
+
+	
+}
---
+++ trangellzarinpal/helper.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @package     Joomla - > Site and Administrator payment info
+ * @subpackage  com_Digicom
+ * @subpackage 	trangell_Zarinpal
+ * @copyright   trangell team => https://trangell.com
+ * @copyright   Copyright (C) 20016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+class plgDigiCom_PayTrangellZarinpalHelper
+{
+
+	public static function Storelog($name,$data)
+	{
+		$my = JFactory::getUser();
+		jimport('joomla.log.log');
+		JLog::addLogger(
+			 array(
+						'text_file' => 'com_digicom.trangellzarinpal.errors.php'
+			 ),
+			 JLog::ALL,
+			 array('com_digicom.trangellzarinpal')
+		 );
+		 $msg = 'StoreLog >>  user:'.$my->name.'('.$my->id.'), desc: ' . json_encode($data['raw_data']);
+		 JLog::add($msg, JLog::WARNING, 'com_digicom.trangellzarinpal');
+
+	}
+
+	public static function getGateMsg ($msgId) {
+		switch($msgId){
+			case	11: $out =  'شماره کارت نامعتبر است';break;
+			case	12: $out =  'موجودي کافي نيست';break;
+			case	13: $out =  'رمز نادرست است';break;
+			case	14: $out =  'تعداد دفعات وارد کردن رمز بيش از حد مجاز است';break;
+			case	15: $out =   'کارت نامعتبر است';break;
+			case	17: $out =   'کاربر از انجام تراکنش منصرف شده است';break;
+			case	18: $out =   'تاريخ انقضاي کارت گذشته است';break;
+			case	21: $out =   'پذيرنده نامعتبر است';break;
+			case	22: $out =   'ترمينال مجوز ارايه سرويس درخواستي را ندارد';break;
+			case	23: $out =   'خطاي امنيتي رخ داده است';break;
+			case	24: $out =   'اطلاعات کاربري پذيرنده نامعتبر است';break;
+			case	25: $out =   'مبلغ نامعتبر است';break;
+			case	31: $out =  'پاسخ نامعتبر است';break;
+			case	32: $out =   'فرمت اطلاعات وارد شده صحيح نمي باشد';break;
+			case	33: $out =   'حساب نامعتبر است';break;
+			case	34: $out =   'خطاي سيستمي';break;
+			case	35: $out =   'تاريخ نامعتبر است';break;
+			case	41: $out =   'شماره درخواست تکراري است';break;
+			case	42: $out =   'تراکنش Sale يافت نشد';break;
+			case	43: $out =   'قبلا درخواست Verify داده شده است';break;
+			case	44: $out =   'درخواست Verify يافت نشد';break;
+			case	45: $out =   'تراکنش Settle شده است';break;
+			case	46: $out =   'تراکنش Settle نشده است';break;
+			case	47: $out =   'تراکنش Settle يافت نشد';break;
+			case	48: $out =   'تراکنش Reverse شده است';break;
+			case	49: $out =   'تراکنش Refund يافت نشد';break;
+			case	51: $out =   'تراکنش تکراري است';break;
+			case	52: $out =   'سرويس درخواستي موجود نمي باشد';break;
+			case	54: $out =   'تراکنش مرجع موجود نيست';break;
+			case	55: $out =   'تراکنش نامعتبر است';break;
+			case	61: $out =   'خطا در واريز';break;
+			case	100: $out =   'تراکنش با موفقيت انجام شد.';break;
+			case	111: $out =   'صادر کننده کارت نامعتبر است';break;
+			case	112: $out =   'خطاي سوئيچ صادر کننده کارت';break;
+			case	113: $out =   'پاسخي از صادر کننده کارت دريافت نشد';break;
+			case	114: $out =   'دارنده کارت مجاز به انجام اين تراکنش نيست';break;
+			case	412: $out =   'شناسه قبض نادرست است';break;
+			case	413: $out =   'شناسه پرداخت نادرست است';break;
+			case	414: $out =   'سازمان صادر کننده قبض نامعتبر است';break;
+			case	415: $out =   'زمان جلسه کاري به پايان رسيده است';break;
+			case	416: $out =   'خطا در ثبت اطلاعات';break;
+			case	417: $out =   'شناسه پرداخت کننده نامعتبر است';break;
+			case	418: $out =   'اشکال در تعريف اطلاعات مشتري';break;
+			case	419: $out =   'تعداد دفعات ورود اطلاعات از حد مجاز گذشته است';break;
+			case	421: $out =   'IP نامعتبر است';break;
+			case	500: $out =   'کاربر به صفحه زرین پال رفته ولي هنوز بر نگشته است';break;
+			case	'error': $out ='خطا غیر منتظره رخ داده است';break;
+			case	'hck2': $out = 'لطفا از کاراکترهای مجاز استفاده کنید';break;
+			case	'notff': $out = 'سفارش پیدا نشد';break;
+			case	'price': $out = 'مبلغ وارد شده کمتر از ۱۰۰۰ ریال می باشد';break;
+			default: $out ='خطا غیر منتظره رخ داده است';break;
+		}
+		return $out;
+	}
+
+	public static function getPayerPrice ($id) {
+		$user = JFactory::getUser();
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select('amount')
+			->from($db->qn('#__digicom_orders'));
+		$query->where(
+			$db->qn('userid') . ' = ' . $db->q($user->id) 
+							. ' AND ' . 
+			$db->qn('id') . ' = ' . $db->q($id)
+		);
+		$db->setQuery((string)$query); 
+		$result = $db->loadResult();
+		return $result;
+	}
+
+
+}
